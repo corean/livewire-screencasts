64d4254b60936c3ffa464431b1eb91f97c14dc66
corean <corean@corean.biz>
2024-01-29 00:30:38 +0900

    - ep8-alpine

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,5 +1,8 @@
 <div>
     <h2>New Post:</h2>
+
+    Current Title (alpinjs) : <span x-html="$wire.title.toUpperCase()"></span>
+    <button type="button" x-on:click="$wire.save()">Save</button>
     <form wire:submit="save">
         <label>
             <span>title</span>
@@ -11,6 +14,7 @@
             <span>content</span>
             <textarea wire:model="content"></textarea>
             @error('content') <span>{{ $message }}</span> @enderror
+            <small>Words: <span x-text="$wire.content.split(' ').length - 1"></span></small>
         </label>
 
         <button type="submit">Save</button>
