b34b3c03851cbd59f038adc4e4355c9b048c6ca6
corean <corean@corean.biz>
2024-01-31 22:18:08 +0900

    - form > ep5. accessibility

--- resources/views/livewire/edit-profile.blade.php
+++ resources/views/livewire/edit-profile.blade.php
@@ -3,35 +3,40 @@
         <h1 class="mb-4 text-2xl text-slate-700 font-semibold">Update your profile...</h1>
 
         <form wire:submit="save" class="min-w-[30rem] flex flex-col gap-6 bg-white rounded-lg shadow p-6">
-            <div class="flex flex-col gap-2">
+            <label class="flex flex-col gap-2">
                 <h3 class="font-medium text-slate-700 text-base">Username</h3>
 
-                <input wire:model.live="form.username"
+                <input wire:model.blur="form.username"
                        @class([
                           'px-3 py-2  rounded-lg',
                           'border border-slate-300' => $errors->missing('form.username'),
                           'border-2 border-red-400' => $errors->has('form.username'),
                       ])
                        placeholder="Username..."
+                       @error('form.username')
+                           aria-invalid="true"
+                           aria-describedby="{{ $meessage }}"
+                       @enderror
                 >
                 @error('form.username')
-                <span class="text-red-400 text-sm">{{ $message }}</span>
+                <span class="text-red-400 text-sm" aria-live="assertive">{{ $message }}</span>
                 @enderror
-            </div>
+            </label>
 
-            <div class="flex flex-col gap-2">
+            <label class="flex flex-col gap-2">
                 <h3 class="font-medium text-slate-700 text-base">Bio</h3>
 
                 <textarea wire:model="form.bio" rows="4" class="px-3 py-2 border border-slate-300 rounded-lg"
                           placeholder="A little bit about yourself..."></textarea>
-            </div>
+            </label>
 
             <div class="flex">
                 <button type="submit"
                         class="relative w-full bg-blue-500 py-3 px-8 rounded-lg text-white font-medium disabled:cursor-not-allowed disabled:opacity-75 "
                 >
                     Save
-                    <div wire:loading.flex wire:target="save" class="flex absolute top-0 right-0 bottom-0 items-center pr-4">
+                    <div wire:loading.flex wire:target="save"
+                         class="flex absolute top-0 right-0 bottom-0 items-center pr-4">
                         <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
                              viewBox="0 0 24 24">
                             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
